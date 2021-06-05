ff2a6d0084cd888f56e482d15b4106af8b3a504a
Bhargav Godhani <bhargavgodhani50@gmail.com>
2021-06-05 17:09:13 +0530

    first

---
+++ function.php
@@ -0,0 +1,29 @@
+<?php
+
+// // add_theme_support('menus');
+
+// // register_nav_menus(
+
+// //     array(
+// //         'top-menu' -> __('Top Menu', 'theme'),
+// //     )
+// //     );
+// function register_my_menus() {
+//     register_nav_menus(
+//       array(
+//         'header-menu' => __( 'Header Menu' ),
+//         'extra-menu' => __( 'Extra Menu' )
+//       )
+//     );
+//   }
+//   add_action( 'init', 'register_my_menus' );
+
+
+
+
+}
+
+
+
+
+?>
